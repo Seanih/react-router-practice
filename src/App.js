eee699c1936ee4342cbd117b63ccf50cdec10d36
Sean Anih <sean.anih@gmail.com>
2020-10-20 20:50:10 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import About from './About';
+import './App.css';
+import Nav from './Nav';
+import Shop from './Shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <About />
+      <Shop />
+    </div>
+  );
+}
+
+export default App;
